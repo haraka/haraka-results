3d479284a1985e775f4cf7abf5ab29b62f0bc53e
Matt Simerson <matt@tnpi.net>
2026-05-27 10:52:07 -0700

    Release v2.3.1 (#45)
    - fix: prevent Prototype pollution in the arbitrary-key loop
    - fix: falsy value loss in both the append_lists and overwrite_lists
    - fix: unhandled Redis promise on the publish() call
    - doc(README): updated description to say the channel uses txn.uuid when processing a message, else conn.uuid

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,3 @@
-import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import haraka from '@haraka/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
-export default [
-  ...compat.extends('@haraka'),
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-]
+export default [...haraka]
